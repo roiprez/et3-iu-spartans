da3d4e061acdd0822134fa7a90c613efdbaf0bbd
roiprez <roiprezl@gmail.com>
2017-11-23 17:49:37 +0100

    Creada estructura de directorios

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/*
+entrada a la aplicacion
+*/
+	//se va usar la session de la conexion
+	session_start();
+	$_SESSION['idioma'] = 'SPANISH';
+
+	//funcion de autenticacion
+	include './Functions/Authentication.php';
+
+	//si no ha pasado por el login de forma correcta
+	if (!IsAuthenticated()){
+		header('Location:./Controllers/Login_Controller.php');
+	}
+	//si ha pasado por el login de forma correcta 
+	else{
+		header('Location:./Controllers/Index_Controller.php');
+	}
+
+	?>
+
+
+
+
+
+
+
