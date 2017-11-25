6e07c74d22087024ab2155956bde30f3fa23b087
roiprez <roiprezl@gmail.com>
2017-11-25 12:37:08 +0100

    Arreglado: Se comentó por error el contenido

--- index.php
+++ index.php
@@ -7,17 +7,17 @@ entrada a la aplicacion
 	session_start();
 	$_SESSION['idioma'] = 'SPANISH';
 
-	// //funcion de autenticacion
-	// include './Functions/Authentication.php';
+	//funcion de autenticacion
+	include './Functions/Authentication.php';
 
-	// //si no ha pasado por el login de forma correcta
-	// if (!IsAuthenticated()){
-	// 	header('Location:./Controllers/Login_Controller.php');
-	// }
-	// //si ha pasado por el login de forma correcta 
-	// else{
+	//si no ha pasado por el login de forma correcta
+	if (!IsAuthenticated()){
+		header('Location:./Controllers/Login_Controller.php');
+	}
+	//si ha pasado por el login de forma correcta 
+	else{
 		
-	// }
+	}
 
 	header('Location:./Controllers/Index_Controller.php');
 
