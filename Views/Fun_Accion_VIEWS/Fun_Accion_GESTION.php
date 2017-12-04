464dd75e5f45dd144188805bff048c064b122e0c
diego <dpdominguez@esei.uvigo.es>
2017-12-04 18:19:26 +0100

    Modificadas las vistas de funcionalidad showall y fun-accion-gestion

--- Views/Fun_Accion_VIEWS/Fun_Accion_GESTION.php
+++ Views/Fun_Accion_VIEWS/Fun_Accion_GESTION.php
@@ -3,14 +3,14 @@
 class Fun_Accion_GESTION// declaración de clase
 {
 //declaracion de atributos
-	var $lista_usuarios;
-	var $lista_grupos;
+	var $funcionalidad;
+	var $lista_acciones;
 	var $lista_valores;
 
     //Constructor
-    function __construct($lista_funcionalidades,$lista_acciones,$lista_valores)
+    function __construct($funcionalidad,$lista_acciones,$lista_valores)
     {	
-    	$this->lista_funcionalidades=$lista_funcionalidades;
+    	$this->funcionalidad=$funcionalidad;
     	$this->lista_valores=$lista_valores;
     	$this->lista_acciones=$lista_acciones;
         $this->pinta();
@@ -21,7 +21,7 @@ class Fun_Accion_GESTION// declaración de clase
 
         ?>
 
-        <form id="formulario-usu_grupo" name="formulario_usu_grupo" method="post">
+        <form id="formulario-fun_accion" name="formulario_fun_accion" method="post">
 
         	<table>
             <tr>
@@ -29,57 +29,49 @@ class Fun_Accion_GESTION// declaración de clase
                 <th>Acciones</th>
             </tr>
 
-            <?php
-            foreach ($this->lista_funcionalidades as $funcionalidad)//recorre todos las funcionalidades creadas creando una fila por funcionalidad
-            {
-                ?>
                     <tr>
                     	<td>
                             <?php echo '$funcionalidad'?>
                             <input type="text" hidden name="IdFuncionalidad"  value="<?php echo '$funcionalidad'?>">
                         </td>
                     	<td>
-                            <select multiple="true">
-                    	<?php
-                            var $acciones_funcionalidad [];
+                            <select  multiple name="IdFuncionalidad[]">
+                        <?php
+                            $acciones_funcionalidad = array();
                             foreach ($this->lista_valores as $tupla) {//recorre el recordset de datos
-                                if ($tupla['IdFuncionalidad']==$funcionalidad) {//almacena en un array con las acciones que pertenecen ya esa funcionalidad
-                                    array_push($acciones_funcionalidad, $tupla['IdAccion'])
+                                if ($tupla['IdFuncionalidad']==$funcionalidad) {//almacena en un array las acciones ya definidas para la funcionalidad
+                                    $acciones_funcionalidad[]=$tupla['IdAccion'];
                                 }//fin if
                             }//fin foreach
-                            
-							for($i=0;$i<count($this->lista_acciones);$i++){//recorre la lista de todas las acciones posibles
+                            for($i=0;$i<count($this->lista_acciones);$i++){//recorre la lista de todas las acciones posibles
 
-                                for($j=0;j<count($acciones_funcionalidad);j++){//recorre las acciones asignadas a esa funcionalidad
-					               if ($acciones_funcionalidad[j]==$this->lista_acciones[$i]) {//si encuentra la accion ya asignada a esa funcionalidad seleccionado=true
-                                       $seleccionado=true;
+                                for($j=0;$j<count($acciones_funcionalidad);$j++){//recorre las acciones de la funcionalidad
+                                   if ($acciones_funcionalidad[$j]==$this->lista_acciones[$i]) {//si encuentra la accion dentro de las acciones ya definidas seleccionado=true
+                                       $sel=true;
                                    }//fin del if
                                 }//fin del bucle for interno
 
-                                if ($seleccionado==true) {//si esta seleccionado opcion seleccionada para esa accion
+                                if ($sel==true) {//si esta seleccionado opcion seleccionada para esa accion
                                     ?>
-                                    <option selected="true" value="<?php echo "$this->lista_acciones[$i]"?>">
-                                        <?php echo "$this->lista_acciones[$i]"?>
+                                    <option selected="true" name="<?php echo $this->lista_acciones[$i] ;?>" value="<?php echo $this->lista_acciones[$i] ;?>">
+                                        <?php echo $this->lista_acciones[$i];?>
                                     </option>
                                     <?php
-                                }else{//si no esta seleccionado opcion normal para ese accion
+                                }else{//si no esta seleccionado opcion normal para esa accion
                                     ?>
-                                    <option value="<?php echo "$this->lista_acciones[$i]"?>">
-                                        <?php echo "$this->lista_acciones[$i]"?>
+                                    <option name="<?php echo $this->lista_acciones[$i] ;?>"  value="<?php echo $this->lista_acciones[$i] ;?>">
+                                        <?php echo $this->lista_acciones[$i] ;?>
                                     </option>
                                     <?php
                                 }//fin del else
-                                $seleccionado=false;//volvemos a poner seleccionado a false
+                                $sel=false;//volvemos a poner seleccionado a false
 
-							}//fin del bucle for externo
-                    	?>
+                            }//fin del bucle for externo
+                        
+                        ?>
                         </select>
-                    	</td>
+                        </td>
                     </tr>
-                
-                <?php
-            }//fin del bucle for each
-            ?>
 
         </table>
 
