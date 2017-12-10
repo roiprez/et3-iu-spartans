047f02f47305558463f6705569c1a7a1b52779a9
Diego <dpersol@hotmail.com>
2017-12-10 17:18:47 +0100

    -Script inicilizador de la BD
    +Este script añade los elementos necesarios para poder comenzar a usar la aplicacion

---
+++ Inicializacion BD.sql
@@ -0,0 +1,48 @@
+/*Insercion del Grupo de Adminitradores*/
+INSERT INTO GRUPO (
+            IdGrupo,
+            NombreGrupo,
+            DescripGrupo)
+            VALUES (
+            'Admin',
+            'Administrador',
+            'Grupo con permisos totales dentro de la aplicacion'
+            );
+/*Insercion del grupo de alumnos*/
+INSERT INTO GRUPO (
+            IdGrupo,
+            NombreGrupo,
+            DescripGrupo)
+            VALUES (
+            'Alumno',
+            'Alumno',
+            'El usuario mas basico de la aplicacion, con permisos muy limitados'
+            );
+/*Insercion de un usuario admin por defecto para poder manipular la configuracion de la aplicacion*/
+INSERT INTO USUARIO (
+					login,
+					password,
+					DNI,
+					Nombre,
+					Apellidos,
+					Correo,
+					Direccion,
+					Telefono)
+						VALUES (
+							'admin',
+							'provisional',
+							'12345678Z',
+							'Admin',
+							'Admin',
+							'admin@admin.com',
+							'adminitracion',
+							'666666666');
+/*Insercion de usuario admin en el grupo de adminitradores*/
+INSERT INTO USU_GRUPO
+                                            (
+                                            login,
+                                            IdGrupo)
+                                            VALUES (
+                                            'admin',
+                                            'Admin'
+                                            )
