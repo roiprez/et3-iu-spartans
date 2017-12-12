dd78c5c1dc53e485080362f03a464c4ed750409e
roiprez <roiprezl@gmail.com>
2017-12-12 23:20:41 +0100

    Arreglos en el controlador de resultados

--- Controllers/Resultados_Controller.php
+++ Controllers/Resultados_Controller.php
@@ -7,54 +7,52 @@ Controlador que se encarga de gestionar las peticiones de lectura y escritura de
 
 include '../Models/EVALUACIONES_Model.php';
 include '../Models/HISTORIAS_Model.php';
+include '../Models/ENTREGAS_Model.php';
 include '../Views/Resultados_VIEWS/Resultados_SHOWCURRENT_ET.php';
 include '../Views/MESSAGE_View.php';
 
 
-if (!isset($_REQUEST['action'])){
-	$_REQUEST['action'] = '';
+
+$IdTrabajo = $_REQUEST['IdTrabajo'];
+$LoginEvaluador = $_SESSION['login']; 
+
+$ENTREGAS = new ENTREGAS_Model($IdTrabajo, $LoginEvaluador, '', '','','', '', '','');
+$entregas = $ENTREGAS->RellenaDatos();
+
+$AliasEvaluado = $entregas[2];
+
+if($IdTrabajo[0] == 'E'){
+  $EVALUACION = new EVALUACIONES_Model($IdTrabajo, '', $AliasEvaluado, '','','', '', '','');
+  $datos = $EVALUACION->SEARCH();
+
+  $HISTORIA = new HISTORIA_Model($IdTrabajo, '', '');
+  $historias = $HISTORIA->SEARCH();
+
+  $descrip_historias = [];
+
+  //Guardamos las descripciones de historias
+  while($row = $historias->fetch_array()) {
+      array_push($descrip_historias, $row[2]);
+  }
+
+  $lista = array('IdTrabajo', 'LoginEvaluador', 'AliasEvaluado', 'IdHistoria', 'CorrectoA', 'ComenIncorrectoA', 'CorrectoP','ComenIncorrectoP','OK');
+  new Resultados_SHOWCURRENT_ET($lista, $datos, $descrip_historias, '../Controllers/Index_Controller.php'); 
+} else {
+  $EVALUACION = new EVALUACIONES_Model($IdTrabajo, $LoginEvaluador, '', '','','', '', '','');
+  $datos = $EVALUACION->SEARCH();
+
+  $HISTORIA = new HISTORIA_Model($IdTrabajo, '', '');
+  $historias = $HISTORIA->SEARCH();
+
+  $descrip_historias = [];
+
+  //Guardamos las descripciones de historias
+  while($row = $historias->fetch_array()) {
+      array_push($descrip_historias, $row[2]);
+  }
+
+  $lista = array('IdTrabajo', 'LoginEvaluador', 'AliasEvaluado', 'IdHistoria', 'CorrectoA', 'ComenIncorrectoA', 'CorrectoP','ComenIncorrectoP','OK');
+  new Resultados_SHOWCURRENT_QA($lista, $datos, $descrip_historias, '../Controllers/Index_Controller.php');	
+
 }
-
-	Switch ($_REQUEST['action']){
-    case 'SHOWCURRENT_ET':
-
-      $EVALUACION = new EVALUACIONES_Model($_REQUEST['IdTrabajo'], '', $_REQUEST['AliasEvaluado'], '','','', '', '','');
-      $datos = $EVALUACION->SEARCH();
-
-      $HISTORIA = new HISTORIA_Model($_REQUEST['IdTrabajo'], '', '');
-      $historias = $HISTORIA->SEARCH();
-
-      $descrip_historias = [];
-
-      //Guardamos las descripciones de historias
-      while($row = $historias->fetch_array()) {
-          array_push($descrip_historias, $row[2]);
-      }
-
-      $lista = array('IdTrabajo', 'LoginEvaluador', 'AliasEvaluado', 'IdHistoria', 'CorrectoA', 'ComenIncorrectoA', 'CorrectoP','ComenIncorrectoP','OK');
-      new Resultados_SHOWCURRENT_ET($lista, $datos, $descrip_historias, '../Controllers/Index_Controller.php');	
-      break;
-      
-    case 'SHOWCURRENT_QA':
-    
-      $EVALUACION = new EVALUACIONES_Model($_REQUEST['IdTrabajo'], $_REQUEST['LoginEvaluador'], '', '','','', '', '','');
-      $datos = $EVALUACION->SEARCH();
-
-      $HISTORIA = new HISTORIA_Model($_REQUEST['IdTrabajo'], '', '');
-      $historias = $HISTORIA->SEARCH();
-
-      $descrip_historias = [];
-
-      //Guardamos las descripciones de historias
-      while($row = $historias->fetch_array()) {
-          array_push($descrip_historias, $row[2]);
-      }
-
-      $lista = array('IdTrabajo', 'LoginEvaluador', 'AliasEvaluado', 'IdHistoria', 'CorrectoA', 'ComenIncorrectoA', 'CorrectoP','ComenIncorrectoP','OK');
-      new Resultados_SHOWCURRENT_QA($lista, $datos, $descrip_historias, '../Controllers/Index_Controller.php');	
-      break;
-			
-    default:
-      					
-	}
 ?>
