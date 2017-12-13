c2ef9edfc5ff89369d001135b3954fb404fd8f01
Dpdominguez <33962418+Dpdominguez@users.noreply.github.com>
2017-12-13 16:31:30 +0100

    Añadida vista de visualizacion de resultados

--- Views/Resultados_VIEWS/Resultados_SHOWCURRENT_ET.php
+++ Views/Resultados_VIEWS/Resultados_SHOWCURRENT_ET.php
@@ -8,16 +8,16 @@
 
 class Resultados_SHOWCURRENT_ET
 {
-    var $lista;
+
     var $datos;
     var $lista_descripHist;
     var $indexphp;
 
     //Constructor de la clase
-    function __construct($lista, $datos,$lista_descripHist, $indexphp){
+    function __construct( $datos,$lista_descripHist, $indexphp){
         //asignación de valores de parámetro a los atributos de la clase
 
-        $this->lista = $lista;
+
         $this->datos = $datos;
         $this->lista_descripHist=$lista_descripHist;
         $this->indexphp = $indexphp;
@@ -26,26 +26,58 @@ class Resultados_SHOWCURRENT_ET
 
     function pinta(){
         //include '../Locales/Strings_'.$_SESSION['idioma'].'.php';
-        if (IsAuthenticated() && !isAdmin()){
+        if (IsAuthenticated()){
             $respuesta= "No tienes permiso para acceder a esta vista";
             new Vista_MESSAGE($respuesta, '../Controllers/Index_Controller.php'); //Mostramos el resultado de la ultima inserción
         }else{
             ?>
-            <table id="tabla-showall">
+            <table id="tabla-resultados">
             <?php
                 for($i=0;count($this->lista_descripHist);$i++){
+                    $correctos=array();
+                    $comentarios=array();
+                    foreach($this->datos as $tupla){//busca en el recordset las cinco correcciones de la historia
+                        if($tupla['idHistoria']==$i){
+                            array_push($correctos,$tupla['correctoA']);//almacena en un array las correcciones
+                            array_push($comentarios,$tupla['ComenIncorrectoA']);//almacena en un array los comentarios
+                            $correctoP=$tupla['correctoP'];
+                            $incorrectoP=$tupla['comenIncorrectoP'];
+                        }
+                    }//fin bucle foreach
             ?>
-                <tr></tr>
-                <tr></tr>
-                <tr></tr>
+                <tr>
+                    <td><?php echo $i ?></td>
+                    <td><?php echo $this->lista_descripHist[$i] ?></td>
+                </tr>
+                <tr>
+                    <td><?php echo $correctoP ?></td>
+                    <?php
+                    for($j=0;$j<5;$j++){
+                        ?>
+                        <td><?php echo $correctos[$j]?></td>
+                        <?php
+                    }
+                    ?>
+
+                </tr>
+                <tr>
+                    <td>
+                    <?php echo $incorrectoP?>
+                    <?php
+                    for($j=0;$j<5;$j++){
+                       echo $comentarios[$j];
+                    }
+                    ?>
+                    </td>
+                </tr>
 
 
                 <?php
                     } //fin bucle for
                 ?>
             </table>
-            <form id="Formulario-mensaje" action="../../Controllers/Index_Controller.php" method="get">
-                <button id="boton-mensaje" type='submit' name='action' title="Volver atrás"><img class="button-td" src="../../Iconos/back.png" ></img></button></form> <!--Imagen para la accion back,que permite volver al menu principal-->
+            <form id="Formulario-mensaje" action="../Controllers/Index_Controller.php" method="get">
+                <button id="boton-mensaje" type='submit' name='action' title="Volver atrás"><img class="button-td" src="../Iconos/back.png" ></img></button></form> <!--Imagen para la accion back,que permite volver al menu principal-->
             <?php
         }//Fin else
     }
