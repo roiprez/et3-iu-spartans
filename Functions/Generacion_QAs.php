2e56368a086aa6daa43c464228d0bcc9ddfd73e5
roiprez <roiprezl@gmail.com>
2017-12-14 18:20:35 +0100

    Añadida asignación de evaluaciones

--- Functions/Generacion_QAs.php
+++ Functions/Generacion_QAs.php
@@ -1,6 +1,8 @@
 <?php
 
 include_once '../Models/ENTREGAS_Model.php';
+include_once '../Models/HISTORIA_Model.php';
+include_once '../Models/EVALUACIONES_Model.php';
 include_once '../Models/ASIGNAC_QA_Model.php';
 // include_once '../Views/MESSAGE_View.php';
 
@@ -26,9 +28,11 @@ function qa_gen($IdTrabajo){
     //Recorre en bucle todos los login
     for($i = 0;$i<count($lista_login);$i++){
       //Asigna como primer alias el quinto que sigue al login
+      //$j = $i+5;
       $j = $i+1;
       $contador = 0;
       //Mientras no haya cinco asignaciones sigue asignando
+      //while($contador < 5){
       while($contador < 2){
         //Comprueba que no haya sobrepasado la variable de control de acceso al tamaño del array
         if($j >= count($lista_alias)){
@@ -40,12 +44,26 @@ function qa_gen($IdTrabajo){
         }
         $ASIGNAC_QA = new ASIGNAC_QA_Model($IdTrabajo, $lista_login[$i],$lista_login_evaluado[$j], $lista_alias[$j]);
         $respuesta = $ASIGNAC_QA->ADD();
-        echo $respuesta;
+        evaluacion_gen($IdTrabajo, $lista_login[$i], $lista_alias[$j]);
+        //echo $respuesta;
 				// new Vista_MESSAGE($respuesta, '../Controllers/Index_Controller.php');
         $contador++;
+        //$j+=5;
         $j+=1; 
       }    
     }
-    return true;
+}
+
+function evaluacion_gen($IdTrabajo, $LoginEvaluador, $AliasEvaluado){
+  $HISTORIAS =  new HISTORIA_Model($IdTrabajo,'','');
+  $historias = $HISTORIAS->SEARCH();
+
+  $lista_historias = [];
+    //Recorre las historias y pasa el Id de historia al modelo de evaluaciones
+    while($row = $historias->fetch_array()) {
+      $EVALUACIONES = new EVALUACIONES_Model($IdTrabajo,$LoginEvaluador,$AliasEvaluado,$row[1],'','','','','');
+      $respuesta = $EVALUACIONES->ADD();
+      echo $respuesta;
+  }
 }
 ?>
