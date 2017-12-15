ac0e8f2dc0965ecb021e1ae5dd63453cb14dce60
roiprez <roiprezl@gmail.com>
2017-12-15 16:46:44 +0100

    Ajustes en la visibilidad de usuarios

--- Views/MenuLateralUsuarios.php
+++ Views/MenuLateralUsuarios.php
@@ -25,10 +25,15 @@ Vista que contiene el Menú lateral
 							</form>
 						</li>
 						<li class="dropdown">
-							<input onclick="dropdownMenu('submenu_resultados')" type="button" name="Controlador" value="Resultados" class="dropbtn"></input>
+							<input onclick="dropdownMenu('submenu_resultados')" type="button" name="Controlador" value="RESULTADOS" class="dropbtn"></input>
 							<ul id="submenu_resultados" class="dropdown-content">
 								<li><a href="../Controllers/Index_Controller.php?Controlador=Resultados&IdTrabajo=ET1">ET1</a></li>
 							</ul>
+						</li>
+						<li class="dropdown">
+							<form action="../Controllers/Index_Controller.php">
+								<input type="submit" name="Controlador" value="NOTAS" onclick="" class="dropbtn"></input>
+							</form>
 						</li>
 					</ul>
 				</aside>
