25752b30113e1978e5302fc79a2db6162d798111
Dpdominguez <33962418+Dpdominguez@users.noreply.github.com>
2017-12-18 11:59:16 +0100

    Añadidos vista de resultados de qa

--- Controllers/Resultados_Controller.php
+++ Controllers/Resultados_Controller.php
@@ -18,55 +18,55 @@ $entregas = $ENTREGAS->RellenaDatos();
 $AliasEvaluado = $entregas[2];
 
 if($IdTrabajo[0] == 'E'){
-  $EVALUACION = new EVALUACIONES_Model($IdTrabajo, '', $AliasEvaluado, '','','', '', '','');
-  $datos = $EVALUACION->SEARCH();
+    $EVALUACION = new EVALUACIONES_Model($IdTrabajo, '', $AliasEvaluado, '','','', '', '','');
+    $datos = $EVALUACION->SEARCH();
 
-  $HISTORIA = new HISTORIA_Model($IdTrabajo, '', '');
-  $historias = $HISTORIA->SEARCH();
-
-  $descrip_historias = [];
-
-  //Guardamos las descripciones de historias
-  while($row = $historias->fetch_array()) {
-      array_push($descrip_historias, $row[2]);
-  }
-
-  $lista = array('IdTrabajo', 'LoginEvaluador', 'AliasEvaluado', 'IdHistoria', 'CorrectoA', 'ComenIncorrectoA', 'CorrectoP','ComentIncorrectoP','OK');
-  new Resultados_SHOWCURRENT_ET($lista, $datos, $descrip_historias, '../Controllers/Index_Controller.php'); 
-} elseif($IdTrabajo[0] == 'Q'){
-  $EVALUACION = new EVALUACIONES_Model($IdTrabajo, $LoginEvaluador, '', '','','', '', '','');
-  $datos = $EVALUACION->SEARCH();
-
-  $HISTORIA = new HISTORIA_Model($IdTrabajo, '', '');
-
-  //Guardará la descripción de las historias
-  $descrip_historias = [];
-  
-  //Guardará el CorrectoA del alumno para las 5 qas
-  $qas = [];
-  //Guardará el OK del profesor para las 5 qas del alumno
-  $oks = [];
-
-  //Recorremos un bucle para las 5 qas
-  for($i=0;$i<5;$i++){
-    //Rellenamos la variable historias
+    $HISTORIA = new HISTORIA_Model($IdTrabajo, '', '');
     $historias = $HISTORIA->SEARCH();
-    //Define el número de historia
-    $j = 0;    
+
+    $descrip_historias = [];
+
+    //Guardamos las descripciones de historias
     while($row = $historias->fetch_array()) {
-      //Guardamos en la qa $i, y en la historia $j el valor de CorrectoA
-      $qas[$i][$j] = $row[4];
-      //Guardamos en la qa $i, y en la historia $j el valor de OK
-      $oks[$i][$j] = $row[8];
-      $j++;
+        array_push($descrip_historias, $row[2]);
     }
-  }
+
+    $lista = array('IdTrabajo', 'LoginEvaluador', 'AliasEvaluado', 'IdHistoria', 'CorrectoA', 'ComenIncorrectoA', 'CorrectoP','ComentIncorrectoP','OK');
+    new Resultados_SHOWCURRENT_ET($lista, $datos, $descrip_historias, '../Controllers/Index_Controller.php');
+} elseif($IdTrabajo[0] == 'Q'){
+    $EVALUACION = new EVALUACIONES_Model($IdTrabajo, $LoginEvaluador, '', '','','', '', '','');
+    $datos = $EVALUACION->SEARCH();
+
+    $HISTORIA = new HISTORIA_Model($IdTrabajo, '', '');
+
+    //Guardará la descripción de las historias
+    $descrip_historias = [];
+  
+    //Guardará el CorrectoA del alumno para las 5 qas
+    $qas = [];
+    //Guardará el OK del profesor para las 5 qas del alumno
+    $oks = [];
+
+    //Recorremos un bucle para las 5 qas
+    for($i=0;$i<5;$i++){
+        //Rellenamos la variable historias
+        $historias = $HISTORIA->SEARCH();
+        //Define el número de historia
+        $j = 0;
+        while($row = $historias->fetch_array()) {
+            //Guardamos en la qa $i, y en la historia $j el valor de CorrectoA
+            $qas[$i][$j] = $row[4];
+            //Guardamos en la qa $i, y en la historia $j el valor de OK
+            $oks[$i][$j] = $row[8];
+            $j++;
+        }
+    }
   
 
-  //Guardamos las descripciones de historias
-  while($row = $historias->fetch_array()) {
-      array_push($descrip_historias, $row[2]);
-  }
-  new Resultados_SHOWCURRENT_QA($qas, $oks, $descrip_historias, '../Controllers/Index_Controller.php');	
+    //Guardamos las descripciones de historias
+    while($row = $historias->fetch_array()) {
+        array_push($descrip_historias, $row[2]);
+    }
+    new Resultados_SHOWCURRENT_QA($qas, $oks, $descrip_historias, '../Controllers/Index_Controller.php');
 }
 ?>
---
+++ Views/Resultados_VIEWS/Resultados_SHOWCURRENT_QA.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Drubito
+ * Date: 18/12/2017
+ * Time: 11:37
+ */
+
+class ResultadosSHOWCURRENT_QA
+{
+    var $QAS;
+    var $OKS;
+
+    var $lista_descripHist;
+    var $indexphp;
+
+    //Constructor de la clase
+    function __construct($QAS,$OKS, $lista_descripHist, $indexphp){
+        //asignación de valores de parámetro a los atributos de la clase
+
+
+        $this->OKS=$OKS;
+        $this->QAS=$QAS;
+        $this->lista_descripHist=$lista_descripHist;
+        $this->indexphp = $indexphp;
+        $this->pinta();
+    }
+
+    function pinta(){
+        include '../Locales/Strings_'.$_SESSION['idioma'].'.php';
+
+        for($i=1;$i<=count($this->lista_descripHist);$i++){//recorre todas las historias, $i=numero de historia actual
+           ?>
+            <table id="tabla-resultados">
+                <tr>
+                    <h3><?php echo $i . "   " . $this->lista_descripHist[$i-1]?></h3>
+                </tr>
+                <tr>
+                    <td></td>
+                    <th>QA1</th>
+                    <th>QA2</th>
+                    <th>QA3</th>
+                    <th>QA4</th>
+                    <th>QA5</th>
+                </tr>
+                <tr>
+                    <td>QA realizadas</td>
+                    <?php
+                    for($j=0;$j<5;$j++){
+                        if($this->QAS[$j][$i]==0){
+                            ?>
+                            <td class="celda-incorrecta"></td>
+                            <?php
+                        } else {
+                            ?>
+                            <td class="celda-correcta"></td>
+                            <?php
+                        }
+                    }
+                    ?>
+                </tr>
+                <tr>
+                    <td>Corrección del profesor</td>
+                    <?php
+                    for($j=0;$j<5;$j++){
+                        if($this->OKS[$j][$i]==0){
+                            ?>
+                            <td class="celda-incorrecta"></td>
+                            <?php
+                        } else {
+                            ?>
+                            <td class="celda-correcta"></td>
+                            <?php
+                        }
+                    }
+                    ?>
+                </tr>
+            </table>
+
+            <?php
+        } //fin bucle for
+        ?>
+
+        <form id="Formulario-mensaje" action="../Controllers/Index_Controller.php" method="get">
+            <button id="boton-mensaje" type='submit' name='action' title="<?php echo $strings['Volver atrás']; ?>"><img class="button-td" src="../Iconos/back.png" ></img></button>
+        </form> <!--Imagen para la accion back,que permite volver al menu principal-->
+        <?php
+
+    }
+}
+
+?>
