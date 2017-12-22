52600542a5570eecd2ddff501ad802fa62c52947
roiprez <roiprezl@gmail.com>
2017-12-22 19:26:27 +0100

    Añadidos comentarios a ficheros

--- Functions/Authentication.php
+++ Functions/Authentication.php
@@ -4,6 +4,7 @@ Devuelve true si el login de la sesión existe
 25/11/2017 por 
 */
 function IsAuthenticated(){
+	//Comprueba que el login existe y devuelve true en caso afirmativo y false en el contrario
 	if (!isset($_SESSION['login'])){	
 		return false;
 	}
--- Functions/comprobarAdmin.php
+++ Functions/comprobarAdmin.php
@@ -6,8 +6,11 @@ Devuelve true si el usuario pertenece al grupo de Admin
 include_once '../Models/GRUPOS_Model.php';
 
 function isAdmin(){
+	//Guarda una nueva instancia de la clase USU_GRUPO_MODEL
 	$GRUPOSLOGIN = new USU_GRUPO_Model($_SESSION['login'],'');
+	//Guarda el resultado de la búsqueda
 	$gruposLogin = $GRUPOSLOGIN->SEARCH();
+	//Mientras haya filas en gruposLogin comprueba que uno de esos grupos sea Admin, en cuyo caso devuelve true, y si llega al final false
 	while($row = $gruposLogin->fetch_array()) {
 		if($row[1] == 'Admin'){
 			return true;
