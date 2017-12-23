85a33f7fb13bd79cf667b0c51286f31e97ffc26c
arnogueira <arnogueira@esei.uvigo.es>
2017-12-23 03:23:17 +0100

    Ahora si la fecha fin de un trabajo ha pasado, desaparece el botón de edicion del trabajo

--- Views/Header.php
+++ Views/Header.php
@@ -11,6 +11,7 @@ Vista que contiene el Header y el menú lateral de la aplicación, en el se cont
 
 		function render(){
 			include_once '../Functions/getTrabajos.php';
+			include_once '../Functions/comprobarFecha.php';
 			include_once '../Functions/Authentication.php';
 			include_once '../Functions/comprobarAdmin.php';
 			include_once '../Functions/Generacion_QAs.php';
